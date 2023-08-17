edaadb71f7ac6c5367d7ecaafe0bb13855ef6d39
Hamza ATILGAN <122400602+hamzaatilgan@users.noreply.github.com>
2023-08-17 17:16:55 +0300

    Portfolio Update

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// Cüneyd BÖLÜKOĞLU 
+// Front-End Developer
+
+$(document).ready(function () {
+
+    new fullpage('.fullpage', {
+        anchors: ['home-section', 'about'],
+
+        //options here
+        autoScrolling: true,
+        scrollHorizontally: true
+    });
+
+});
